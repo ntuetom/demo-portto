f349dca994d560f452b41a979dae5438e20f98f3
Wu hung-yi <hyha1013@hotmail.com>
2021-03-08 04:57:35 +0800

    firstcommit

---
+++ ERCCollection/ERCCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ERCCollection
+//
+//  Created by Wu hung-yi on 2021/3/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
